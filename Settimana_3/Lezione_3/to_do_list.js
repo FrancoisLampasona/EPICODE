17c660c90382a79b5ae49e87932496b6a04f002f
Francois Lampasona <lampasonafrancois@gmail.com>
2024-10-16 17:37:07 +0200

    Settimana_3_Lezione_3

--- Settimana_3/Lezione_3/to_do_list.js
+++ Settimana_3/Lezione_3/to_do_list.js
@@ -2,23 +2,34 @@ const form_input = document.getElementsByTagName("form")[0];
 
 form_input.addEventListener("submit", function (e) {
   e.preventDefault();
+
   const task_text = document.getElementById("task_text").value;
+
   const task_section = document.getElementById("task_section");
+
   const card = document.createElement("div");
+
   card.classList.add("contact-card");
+
   card.innerHTML = `<p>${task_text}</p>
-  <button type = 'button' onclick = 'completa_task(this)'>Completata</button>
-  <button type = 'button' onclick = 'delete_task(this)'>Cancella Task</button>`;
+  
+    <button type = 'button' onclick = 'completa_task(event)'>Completata</button>
+  
+    <button type = 'button' onclick = 'delete_task(event)'>Cancella Task</button>`;
+
   task_section.appendChild(card);
+
   form_input.reset();
 });
 
-const completa_task = function (button) {
-  const taskText = button.parentElement.querySelector("p");
+const completa_task = function (evento) {
+  const taskText = evento.target.parentElement.querySelector("p");
+
   taskText.style.textDecoration = "line-through";
 };
 
-const delete_task = function (button) {
-  const card = button.parentElement;
+const delete_task = function (evento) {
+  const card = evento.target.parentElement;
+
   card.remove();
 };
