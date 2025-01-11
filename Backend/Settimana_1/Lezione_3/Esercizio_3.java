e6e334a4e365c2ab43f37abec5555c377f991f6a
Francois Lampasona <lampasonafrancois@gmail.com>
2025-01-11 14:25:24 +0100

    Settimana_1_Lezione_3

--- Backend/Settimana_1/Lezione_3/Esercizio_3.java
+++ Backend/Settimana_1/Lezione_3/Esercizio_3.java
@@ -9,34 +9,118 @@
 
 package Lezione_3;
 
-
-class Articolo{
+class Articolo {
     private int codiceArticolo;
     private String descrizioneArticolo;
     private double prezzoArticolo;
-    private int numeroPezziDisponibili; 
+    private int numeroPezziDisponibili;
 
+    // Costruttore
+    public Articolo(int codiceArticolo, String descrizioneArticolo, double prezzoArticolo, int numeroPezziDisponibili) {
+        this.codiceArticolo = codiceArticolo;
+        this.descrizioneArticolo = descrizioneArticolo;
+        this.numeroPezziDisponibili = numeroPezziDisponibili;
+        this.prezzoArticolo = prezzoArticolo;
+    }
+
+    // Getter per il prezzo dell'articolo
+    public double getPrezzoArticolo() {
+        return prezzoArticolo;
+    }
+
+    @Override
+    public String toString() {
+        return "Articolo{" +
+                "codiceArticolo=" + codiceArticolo +
+                ", descrizioneArticolo='" + descrizioneArticolo + '\'' +
+                ", prezzoArticolo=" + prezzoArticolo +
+                ", numeroPezziDisponibili=" + numeroPezziDisponibili +
+                '}';
+    }
 }
 
 class Cliente {
-
     private int codiceCliente;
     private String nome;
     private String cognome;
     private String email;
     private String dataIscrizione;
 
+    // Costruttore
+    public Cliente(int codiceCliente, String nome, String cognome, String email, String dataIscrizione) {
+        this.codiceCliente = codiceCliente;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.dataIscrizione = dataIscrizione;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "codiceCliente=" + codiceCliente +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataIscrizione='" + dataIscrizione + '\'' +
+                '}';
+    }
 }
 
 class Carrello {
     private Cliente clienteAssociato;
-    private Articolo [] listaArticoli;
-    private int costoTotale;
+    private Articolo[] listaArticoli;
+    private double costoTotale;
 
+    // Costruttore
+    public Carrello(Cliente clienteAssociato, Articolo[] listaArticoli) {
+        this.clienteAssociato = clienteAssociato;
+        this.listaArticoli = listaArticoli;
+        this.costoTotale = calcolaCostoTotale();
+    }
+
+    private double calcolaCostoTotale() {
+        double totale = 0;
+        if (listaArticoli != null) {
+            for (Articolo articolo : listaArticoli) {
+                totale += articolo.getPrezzoArticolo();
+            }
+        }
+        return totale;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder articoli = new StringBuilder();
+        if (listaArticoli != null) {
+            for (Articolo articolo : listaArticoli) {
+                articoli.append(articolo.toString()).append("\n");
+            }
+        }
+        return "Carrello{" +
+                "clienteAssociato=" + clienteAssociato +
+                ", listaArticoli=\n" + articoli +
+                ", costoTotale=" + costoTotale +
+                '}';
+    }
 }
 
-
-
 public class Esercizio_3 {
-    
+    public static void main(String[] args) {
+        // Crea un cliente
+        Cliente cliente = new Cliente(1, "Mario", "Rossi", "mario.rossi@example.com", "2023-01-01");
+
+        // Crea alcuni articoli
+        Articolo articolo1 = new Articolo(101, "Penna", 1.50, 100);
+        Articolo articolo2 = new Articolo(102, "Quaderno", 2.00, 50);
+
+        // Crea una lista di articoli
+        Articolo[] listaArticoli = { articolo1, articolo2 };
+
+        // Crea un carrello associato al cliente
+        Carrello carrello = new Carrello(cliente, listaArticoli);
+
+        // Stampa il contenuto del carrello
+        System.out.println(carrello);
+    }
 }
