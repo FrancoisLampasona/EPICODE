de3b9bcdd9d070ab01a9a678068b4d144ea71db2
Francois Lampasona <lampasonafrancois@gmail.com>
2025-02-14 09:33:44 +0100

    Settimana_6

---
+++ Backend/Settimana_6/progetto_settimanale/src/test/java/it/francois/progetto_settimanale/ProgettoSettimanaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.francois.progetto_settimanale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
